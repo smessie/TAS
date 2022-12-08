3dca48d9751e7abf5f6e546c6b4b6409138ce620
smessie <smessie@smessie.com>
2022-12-08 14:11:54 +0100

    feat: Add authentication

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,6 +2,31 @@
   <MDBContainer>
     <h1>👜 TAS - your Todo App with Solid</h1>
 
+    <MDBCard>
+      <MDBCardBody class="w-100">
+        <MDBCardTitle>Authentication</MDBCardTitle>
+        <MDBCardText>
+          <div v-if="loggedIn">
+            <p class="text-success">
+              You are logged in as <em>{{ loggedIn }}</em
+            >.
+            </p>
+            <MDBBtn color="primary" @click="logout">Logout</MDBBtn>
+          </div>
+          <div v-else>
+            <MDBInput
+              v-model="oidcIssuer"
+              label="OIDC Issuer"
+              type="text"
+              required
+              style="margin-bottom: 1rem"
+            />
+            <MDBBtn color="primary" @click="login">Login</MDBBtn>
+          </div>
+        </MDBCardText>
+      </MDBCardBody>
+    </MDBCard>
+
     <form @submit="addTodo">
       <MDBInput
         inputGroup
@@ -46,7 +71,18 @@ import {
   MDBListGroupItem,
   MDBInput,
   MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCardText,
+  MDBCardTitle,
 } from "mdb-vue-ui-kit";
+import {
+  getDefaultSession,
+  handleIncomingRedirect,
+  login,
+  fetch,
+  logout,
+} from "@inrupt/solid-client-authn-browser";
 
 export default {
   name: "TodoList",
@@ -56,11 +92,17 @@ export default {
     MDBListGroupItem,
     MDBInput,
     MDBBtn,
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
   },
   data() {
     return {
       newTodo: "",
       todos: [],
+      loggedIn: undefined,
+      oidcIssuer: "",
     };
   },
   created() {
@@ -71,6 +113,12 @@ export default {
       "Porta ac consectetur ac",
       "Vestibulum at eros",
     ];
+
+    handleIncomingRedirect({
+      restorePreviousSession: true,
+    }).then((info) => {
+      this.loggedIn = info.webId;
+    });
   },
   methods: {
     addTodo(event) {
@@ -80,6 +128,27 @@ export default {
         this.todos.push(this.newTodo);
         this.newTodo = "";
       }
+    },
+    async login() {
+      await handleIncomingRedirect();
+
+      // 2. Start the Login Process if not already logged in.
+      if (!getDefaultSession().info.isLoggedIn) {
+        await login({
+          // Specify the URL of the user's Solid Identity Provider;
+          // e.g., "https://login.inrupt.com".
+          oidcIssuer: this.oidcIssuer,
+          // Specify the URL the Solid Identity Provider should redirect the user once logged in,
+          // e.g., the current page for a single-page app.
+          redirectUrl: window.location.href,
+          // Provide a name for the application when sending to the Solid Identity Provider
+          clientName: "TAS",
+        });
+      }
+    },
+    async logout() {
+      await logout();
+      this.loggedIn = undefined;
     },
   },
 };
@@ -92,5 +161,8 @@ export default {
 h1 {
   margin-top: 3rem;
   margin-bottom: 3rem;
+}
+.card {
+  margin-bottom: 2rem;
 }
 </style>
